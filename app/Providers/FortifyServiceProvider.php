9ef2e8828e1a29ab83050ba6d2847827266f46c3
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2024-12-16 00:20:57 +0200

    Fortify files cleaning

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -8,9 +8,11 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Contracts\UpdatesUserPasswords;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -19,7 +21,26 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+      $this->app->bind(UpdateUserPassword::class, function ($app) {
+        return new class implements UpdatesUserPasswords {
+            public function update(User $user, array $input): void
+            {
+              Validator::make($input, [
+                  'current_password' => ['required'],
+                  'password' => ['required', 'string', 'min:8'],
+                  'password_confirmation' => ['required', 'string', 'min:8'],
+              ])->after(function ($validator) use ($user, $input) {
+                  if (! Hash::check($input['current_password'], $user->password)) {
+                    $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
+                  }
+              })->validate();
+
+              $user->forceFill([
+                  'password' => Hash::make($input['password']),
+              ])->save();
+            }
+        };
+    });
     }
 
     /**
